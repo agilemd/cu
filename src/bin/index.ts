60ec443e02f95945c9cbafc0174744b038f9b98d
junosuarez <juno@suarez.email>
2019-04-05 19:42:45 -0700

    Pave

---
+++ src/bin/index.ts
@@ -0,0 +1,5 @@
+function main(argv: string[]) {
+  console.log('nothing to see here')
+}
+
+main(process.argv)
---
+++ src/test/test.ts
@@ -0,0 +1,5 @@
+describe("cu", () => {
+  it("works", () => {
+    console.log("ok :)");
+  });
+});
